38433bf31935acde8f56bc811e60c78d4f952889
Gianni Carafa <gianni.carafa@srf.ch>
2021-08-05 08:02:57 +0200

    WIP recieving webhooks on same port but on the Path /webhooks. Working, but code belongs somewhere else.

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ try {
 
     const configureAPI = require('./configure');
 
+
+    if (process.env.GITHUB_APP_WEBHOOK_SECRET) {
+        console.log('webhook listening on /webhook');
+
+        const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
+        const github = require('./github');
+    
+        const webhooks = new Webhooks({
+            secret: process.env.GITHUB_APP_WEBHOOK_SECRET,
+        });
+    
+        webhooks.on('workflow_run', ({ id, name, payload }) => {
+            console.log(`workflow_run received id: ${id}, name: ${name}`, payload);
+            github.mergeRuns([{
+                runId: payload.workflow_run.id,
+                repo: payload.workflow_run.repository.name,
+                owner: payload.workflow_run.repository.owner.login,
+                workflowId: payload.workflow_run.workflow_id,
+                runNumber: payload.workflow_run.run_number,
+                workflow: payload.workflow_run.name,
+                branch: payload.workflow_run.head_branch,
+                sha: payload.workflow_run.head_sha,
+                message: payload.workflow_run.head_commit.message,
+                committer: payload.workflow_run.head_commit.committer.name,
+                status: payload.workflow_run.status === 'completed' ? payload.workflow_run.conclusion : payload.workflow_run.status,
+                createdAt: payload.workflow_run.created_at,
+                updatedAt: payload.workflow_run.updated_at
+            }]);
+        });
+        
+        const middleware = createNodeMiddleware(webhooks, { path: "/webhook" });
+        app.use('/webhook', middleware);
+        //app.all('/webhook', webhooks.middleware(req, res));
+
+    }
     const { PORT = 8080 } = process.env;
 
     // API
